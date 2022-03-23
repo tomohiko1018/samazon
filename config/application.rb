0d09efb42398a7c3580d54d331601b4e5ad816eb
tomohiko1018 <tomohiko1018.n@gmail.com>
2022-03-23 04:05:25 +0000

    create score_to_reviews

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,16 @@ module Samazon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
-    #Sconfig.i18n.default_locale = :ja
+
+    config.i18n.default_locale = :ja
+
+     config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # バリデーションエラーが発生したときに'field_with_errors'いうclassを付加しない
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
-    config.assets.initialize_on_precompile = false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_133405) do
+ActiveRecord::Schema.define(version: 2022_03_23_023825) do
 
   create_table "categories", force: :cascade do |t|
     t.string "major_category_name"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2022_03_19_133405) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score", default: 0
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
