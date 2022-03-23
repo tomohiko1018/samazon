a8f4b849e46083f6ac603ea4982b8cb177a2fa76
tomohiko1018 <tomohiko1018.n@gmail.com>
2022-03-23 02:45:59 +0000

    first half end

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Samazon
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    config.assets.initialize_on_precompile = false
   end
 end
---
+++ db/migrate/20220323023825_add_score_to_reviews.rb
@@ -0,0 +1,4 @@
+class AddScoreToReviews < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
