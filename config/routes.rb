3512e7c505326171a062614e9916a2e5e7ebe7ae
tomohiko1018 <tomohiko1018.n@gmail.com>
2022-03-31 09:13:43 +0000

    create history cart

--- config/routes.rb
+++ config/routes.rb
@@ -42,17 +42,19 @@ Rails.application.routes.draw do
   root :to => "web#index"
   resource :users, only: [:edit, :update] do
     collection do
-      get "cart", :to => "shopping_carts#index"
-      post "cart/create", :to => "shopping_carts#create"
-      delete "cart", :to => "shopping_carts#destroy"
-      get "mypage", :to =>  "users#mypage"
-      get "mypage/edit", :to => "users#edit"
-      get "mypage/address/edit", :to => "users#edit_address"
-      put "mypage", :to => "users#update"
-      get "mypage/edit_password", :to =>"users#edit_password"
-      put "mypage/password", :to => "users#update_password"
-      get "mypage/favorite", :to => "users#favorite"
-      delete "mypage/delete", :to => "users#destroy"
+      get    "cart",                 :to => "shopping_carts#index"
+      post   "cart/create",          :to => "shopping_carts#create"
+      delete "cart",                 :to => "shopping_carts#destroy"
+      get    "mypage",               :to =>  "users#mypage"
+      get    "mypage/edit",          :to => "users#edit"
+      get    "mypage/address/edit",  :to => "users#edit_address"
+      put    "mypage",               :to => "users#update"
+      get    "mypage/edit_password", :to =>"users#edit_password"
+      put    "mypage/password",      :to => "users#update_password"
+      get    "mypage/favorite",      :to => "users#favorite"
+      delete "mypage/delete",        :to => "users#destroy"
+      get    "mypage/cart_history",  :to => "users#cart_history_index", :as => "mypage_cart_histories"
+      get    "mypage/cart_history/:num", :to => "users#cart_history_show", :as =>"mypage_cart_history"
     end
   end
     
